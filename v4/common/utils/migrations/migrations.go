cea1e4a722d144632701c317ca77c2ab7be0c083
charles <charles@pydio.com>
2021-11-03 12:39:04 +0100

    Import common/migrations

---
+++ v4/common/utils/migrations/migrations.go
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2019-2021. Abstrium SAS <team (at) pydio.com>
+ * This file is part of Pydio Cells.
+ *
+ * Pydio Cells is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio Cells is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio Cells.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <https://pydio.com>.
+ */
+
+package migrations
+
+import (
+	"context"
+
+	"github.com/hashicorp/go-version"
+)
+
+// Migration defines a target version and functions to upgrade and/or downgrade.
+type Migration struct {
+	TargetVersion *version.Version
+	Up            func(ctx context.Context) error
+	Down          func(ctx context.Context) error
+}
+
+// FirstRun returns version "zero".
+func FirstRun() *version.Version {
+	obj, _ := version.NewVersion("0.0.0")
+	return obj
+}
+
+// Apply browses migrations upward on downward and apply them sequentially. It returns a version to be
+// saved as the current valid version of the service, or nil if no changes were necessary. In specific case where
+// current version is 0.0.0 (first run), it only applies first run migration (if any) and returns target version.
+func Apply(ctx context.Context, current *version.Version, target *version.Version, migrations []*Migration) (*version.Version, error) {
+
+	if migrations == nil {
+		return target, nil
+	}
+
+	if target.GreaterThan(current) {
+		var successVersion *version.Version
+		for _, migration := range migrations {
+			v := migration.TargetVersion
+			if migration.Up != nil && (current.String() == "0.0.0" || v.GreaterThan(current)) && (v.LessThan(target) || v.Equal(target)) {
+				if err := migration.Up(ctx); err != nil {
+					return successVersion, err
+				}
+				successVersion, _ = version.NewVersion(v.String())
+			}
+		}
+	}
+
+	if target.LessThan(current) {
+		var successVersion *version.Version
+		for i := len(migrations) - 1; i >= 0; i-- {
+			migration := migrations[i]
+			v := migration.TargetVersion
+			if migration.Down != nil && v.GreaterThan(target) && (v.LessThan(current) || v.Equal(current)) {
+				if err := migration.Down(ctx); err != nil {
+					return successVersion, err
+				}
+				successVersion, _ = version.NewVersion(v.String())
+			}
+		}
+
+	}
+
+	return target, nil
+}
--- v4/go.mod
+++ v4/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/ajvb/kala v0.8.4 // indirect
+	github.com/allegro/bigcache v1.2.1 // indirect
 	github.com/beevik/ntp v0.2.0
 	github.com/caddyserver/caddy v1.0.5
 	github.com/codegangsta/cli v1.8.1-0.20150711215404-bca61c476e3c // indirect
@@ -13,6 +14,7 @@ require (
 	github.com/go-openapi/loads v0.21.0
 	github.com/go-openapi/spec v0.20.4
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.1.2
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
@@ -21,6 +23,7 @@ require (
 	github.com/hashicorp/go-version v1.2.1
 	github.com/jaytaylor/go-hostsfile v0.0.0-20201026230151-f581673a59cf
 	github.com/json-iterator/go v1.1.11
+	github.com/karrick/godirwalk v1.16.1 // indirect
 	github.com/micro/go-log v0.1.0
 	github.com/micro/go-micro v1.18.0
 	github.com/micro/micro/v3 v3.6.0
@@ -37,6 +40,7 @@ require (
 	github.com/pydio/config-srv v0.0.0-20180410161705-64a62c922b84 // indirect
 	github.com/pydio/go-os v0.0.0-20180410161642-493f22970962
 	github.com/pydio/minio-go v3.0.1+incompatible
+	github.com/rjeczalik/notify v0.9.2 // indirect
 	github.com/robertkrimen/otto v0.0.0-20211024170158-b87d35c0b86f
 	github.com/rs/cors v1.7.0
 	github.com/shibukawa/configdir v0.0.0-20170330084843-e180dbdc8da0
@@ -49,6 +53,7 @@ require (
 	go.etcd.io/bbolt v1.3.5
 	go.uber.org/zap v1.17.0
 	golang.org/x/net v0.0.0-20210421230115-4e50805a0758
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20210903162649-d08c68adba83
 	google.golang.org/grpc v1.40.0
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
