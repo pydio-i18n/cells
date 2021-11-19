429eb92d7ea73311423b740ad25580be4504364e
charles <charles@pydio.com>
2021-11-19 17:20:48 +0100

    Comment out all services for now

--- v4/main.go
+++ v4/main.go
@@ -16,85 +16,71 @@ import (
 	_ "github.com/pydio/cells/v4/discovery/registry"
 	_ "github.com/pydio/cells/v4/discovery/update/grpc"
 	_ "github.com/pydio/cells/v4/discovery/update/rest"
-
 	// Data
-	_ "github.com/pydio/cells/v4/data/docstore/grpc"
-	_ "github.com/pydio/cells/v4/data/key/grpc"
-	_ "github.com/pydio/cells/v4/data/meta/grpc"
-	_ "github.com/pydio/cells/v4/data/meta/rest"
-	_ "github.com/pydio/cells/v4/data/search/grpc"
-	_ "github.com/pydio/cells/v4/data/search/rest"
-	_ "github.com/pydio/cells/v4/data/source/index"
-	_ "github.com/pydio/cells/v4/data/source/index/grpc"
-	_ "github.com/pydio/cells/v4/data/source/objects"
-	_ "github.com/pydio/cells/v4/data/source/objects/grpc"
-	_ "github.com/pydio/cells/v4/data/source/sync"
-	_ "github.com/pydio/cells/v4/data/source/sync/grpc"
-	_ "github.com/pydio/cells/v4/data/templates/rest"
-	_ "github.com/pydio/cells/v4/data/tree/grpc"
-	_ "github.com/pydio/cells/v4/data/tree/rest"
-	_ "github.com/pydio/cells/v4/data/versions/grpc"
-
-	// Broker
-	_ "github.com/pydio/cells/v4/broker/activity/grpc"
-	_ "github.com/pydio/cells/v4/broker/activity/rest"
-	_ "github.com/pydio/cells/v4/broker/chat/grpc"
-	_ "github.com/pydio/cells/v4/broker/log/grpc"
-	_ "github.com/pydio/cells/v4/broker/log/rest"
-	_ "github.com/pydio/cells/v4/broker/mailer/grpc"
-	_ "github.com/pydio/cells/v4/broker/mailer/rest"
-
-	// Gateways
-	_ "github.com/pydio/cells/v4/gateway/data"
-	_ "github.com/pydio/cells/v4/gateway/dav"
-	_ "github.com/pydio/cells/v4/gateway/proxy"
-	_ "github.com/pydio/cells/v4/gateway/websocket"
-	_ "github.com/pydio/cells/v4/gateway/wopi"
-
-	// IDM
-	_ "github.com/pydio/cells/v4/idm/acl/grpc"
-	_ "github.com/pydio/cells/v4/idm/acl/rest"
-	_ "github.com/pydio/cells/v4/idm/graph/rest"
-	_ "github.com/pydio/cells/v4/idm/key/grpc"
-	_ "github.com/pydio/cells/v4/idm/meta/grpc"
-	_ "github.com/pydio/cells/v4/idm/meta/rest"
-	_ "github.com/pydio/cells/v4/idm/oauth/grpc"
-	_ "github.com/pydio/cells/v4/idm/oauth/rest"
-	_ "github.com/pydio/cells/v4/idm/policy/grpc"
-	_ "github.com/pydio/cells/v4/idm/policy/rest"
-	_ "github.com/pydio/cells/v4/idm/role/grpc"
-	_ "github.com/pydio/cells/v4/idm/role/rest"
-	_ "github.com/pydio/cells/v4/idm/share/rest"
-	_ "github.com/pydio/cells/v4/idm/user/grpc"
-	_ "github.com/pydio/cells/v4/idm/user/rest"
-	_ "github.com/pydio/cells/v4/idm/workspace/grpc"
-	_ "github.com/pydio/cells/v4/idm/workspace/rest"
-
-	// Scheduler
-	_ "github.com/pydio/cells/v4/scheduler/jobs/grpc"
-	_ "github.com/pydio/cells/v4/scheduler/jobs/rest"
-	_ "github.com/pydio/cells/v4/scheduler/tasks/grpc"
-	_ "github.com/pydio/cells/v4/scheduler/timer/grpc"
-
-	// Scheduler Actions
-	_ "github.com/pydio/cells/v4/broker/activity/actions"
-	_ "github.com/pydio/cells/v4/scheduler/actions/archive"
-	_ "github.com/pydio/cells/v4/scheduler/actions/cmd"
-	_ "github.com/pydio/cells/v4/scheduler/actions/idm"
-	_ "github.com/pydio/cells/v4/scheduler/actions/images"
-	_ "github.com/pydio/cells/v4/scheduler/actions/scheduler"
-	_ "github.com/pydio/cells/v4/scheduler/actions/tools"
-	_ "github.com/pydio/cells/v4/scheduler/actions/tree"
-	//
-	// Gateway Micro (Rest API)
-	// Not running yet, defaults.Registry() nil
-	// _ "github.com/pydio/cells/v4/gateway/micro"
-	//
-	//
-	// Minio Starts compiling, running!
-	// As OBJECT
+	//_ "github.com/pydio/cells/v4/data/docstore/grpc"
+	//_ "github.com/pydio/cells/v4/data/key/grpc"
+	//_ "github.com/pydio/cells/v4/data/meta/grpc"
+	//_ "github.com/pydio/cells/v4/data/meta/rest"
+	//_ "github.com/pydio/cells/v4/data/search/grpc"
+	//_ "github.com/pydio/cells/v4/data/search/rest"
+	//_ "github.com/pydio/cells/v4/data/source/index"
+	//_ "github.com/pydio/cells/v4/data/source/index/grpc"
+	//_ "github.com/pydio/cells/v4/data/source/objects"
 	//_ "github.com/pydio/cells/v4/data/source/objects/grpc"
-	// AS GATEWAY
+	//_ "github.com/pydio/cells/v4/data/source/sync"
+	//_ "github.com/pydio/cells/v4/data/source/sync/grpc"
+	//_ "github.com/pydio/cells/v4/data/templates/rest"
+	//_ "github.com/pydio/cells/v4/data/tree/grpc"
+	//_ "github.com/pydio/cells/v4/data/tree/rest"
+	//_ "github.com/pydio/cells/v4/data/versions/grpc"
+	// Broker
+	//_ "github.com/pydio/cells/v4/broker/activity/grpc"
+	//_ "github.com/pydio/cells/v4/broker/activity/rest"
+	//_ "github.com/pydio/cells/v4/broker/chat/grpc"
+	//_ "github.com/pydio/cells/v4/broker/log/grpc"
+	//_ "github.com/pydio/cells/v4/broker/log/rest"
+	//_ "github.com/pydio/cells/v4/broker/mailer/grpc"
+	//_ "github.com/pydio/cells/v4/broker/mailer/rest"
+	// Gateways
+	//_ "github.com/pydio/cells/v4/gateway/data"
+	//_ "github.com/pydio/cells/v4/gateway/dav"
+	//_ "github.com/pydio/cells/v4/gateway/proxy"
+	//_ "github.com/pydio/cells/v4/gateway/websocket"
+	//_ "github.com/pydio/cells/v4/gateway/wopi"
+	// TODO V4 Not running yet, defaults.Registry() nil
+	// _ "github.com/pydio/cells/v4/gateway/micro"
+	// IDM
+	//_ "github.com/pydio/cells/v4/idm/acl/grpc"
+	//_ "github.com/pydio/cells/v4/idm/acl/rest"
+	//_ "github.com/pydio/cells/v4/idm/graph/rest"
+	//_ "github.com/pydio/cells/v4/idm/key/grpc"
+	//_ "github.com/pydio/cells/v4/idm/meta/grpc"
+	//_ "github.com/pydio/cells/v4/idm/meta/rest"
+	//_ "github.com/pydio/cells/v4/idm/oauth/grpc"
+	//_ "github.com/pydio/cells/v4/idm/oauth/rest"
+	//_ "github.com/pydio/cells/v4/idm/policy/grpc"
+	//_ "github.com/pydio/cells/v4/idm/policy/rest"
+	//_ "github.com/pydio/cells/v4/idm/role/grpc"
+	//_ "github.com/pydio/cells/v4/idm/role/rest"
+	//_ "github.com/pydio/cells/v4/idm/share/rest"
+	//_ "github.com/pydio/cells/v4/idm/user/grpc"
+	//_ "github.com/pydio/cells/v4/idm/user/rest"
+	//_ "github.com/pydio/cells/v4/idm/workspace/grpc"
+	//_ "github.com/pydio/cells/v4/idm/workspace/rest"
+	// Scheduler
+	//_ "github.com/pydio/cells/v4/scheduler/jobs/grpc"
+	//_ "github.com/pydio/cells/v4/scheduler/jobs/rest"
+	//_ "github.com/pydio/cells/v4/scheduler/tasks/grpc"
+	//_ "github.com/pydio/cells/v4/scheduler/timer/grpc"
+	// Scheduler Actions
+	//_ "github.com/pydio/cells/v4/broker/activity/actions"
+	//_ "github.com/pydio/cells/v4/scheduler/actions/archive"
+	//_ "github.com/pydio/cells/v4/scheduler/actions/cmd"
+	//_ "github.com/pydio/cells/v4/scheduler/actions/idm"
+	//_ "github.com/pydio/cells/v4/scheduler/actions/images"
+	//_ "github.com/pydio/cells/v4/scheduler/actions/scheduler"
+	//_ "github.com/pydio/cells/v4/scheduler/actions/tools"
+	//_ "github.com/pydio/cells/v4/scheduler/actions/tree"
 )
 
 func main() {
